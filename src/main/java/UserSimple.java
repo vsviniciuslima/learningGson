4b4765f5df36c03805851667a8fba2f6490c51c1
Vinicius <vsviniciuslima@usp.br>
2020-11-14 21:09:50 -0300

    add serialization and deserialization methods

---
+++ src/main/java/UserSimple.java
@@ -0,0 +1,21 @@
+public class UserSimple {
+    private String name;
+    private String email;
+    private int age;
+    private boolean isDeveloper;
+
+    public UserSimple(String name, String email, int age, boolean isDeveloper) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.isDeveloper = isDeveloper;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+}
--- src/main/java/learningJson.java
+++ src/main/java/learningJson.java
@@ -1,6 +1,40 @@
+import com.google.gson.Gson;
+
 public class learningJson {
 
     public static void main(String[] args) {
-        System.out.println("running maven with all dependencies");
+
+        //serializeUserSimple();
+        deserializeUserSimple();
+    }
+
+    private static void serializeUserSimple() {
+
+        UserSimple user = new UserSimple(
+                "Vinicius",
+                "vsviniciuslima@gmail.com",
+                20,
+                true
+                );
+
+        Gson gson = new Gson();
+
+        String json = gson.toJson(user);
+
+    }
+
+    private static void deserializeUserSimple() {
+        String source = "{\n" +
+                "  \"name\": \"Guilherme\",\n" +
+                "  \"email\": \"gsguilhermelima@gmail.com\",\n" +
+                "  \"age\": 23,\n" +
+                "  \"isDeveloper\": false\n" +
+                "}";
+
+        Gson gson = new Gson();
+
+        UserSimple user2 = gson.fromJson(source, UserSimple.class);
+
+        System.out.println(user2.getName() + user2.getAge());
     }
 }
