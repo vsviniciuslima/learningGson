568158f45cd132f91509ac35b3a6b9074f086739
Vinicius <vsviniciuslima@usp.br>
2020-11-15 14:08:26 -0300

    RuntimeTypeAdapterFactory implementation

--- src/main/java/learningJson.java
+++ src/main/java/learningJson.java
@@ -1,4 +1,7 @@
-import com.google.gson.Gson;
+import com.google.gson.*;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class learningJson {
 
@@ -7,9 +10,22 @@ public class learningJson {
         //serializeUserSimple();
         //deserializeUserSimple();
 
-        System.out.println("learningGson 2.0");
+        //System.out.println("learningGson 2.0");
         //serializeUserNested();
-        deserializeUserNested();
+        //deserializeUserNested();
+
+        Branch root = createNode();
+        assemble(root);
+
+        serializeTree(root);
+
+        Branch newRoot = serializeTree(root);
+        assemble(newRoot);
+
+        Gson gson = new Gson();
+
+        String rootOutput = gson.toJson(root);
+        String newRootOutput = gson.toJson(newRoot);
     }
 
     private static void serializeUserSimple() {
@@ -83,5 +99,52 @@ public class learningJson {
                 "}";
 
         Restaurant restaurant = new Gson().fromJson(restaurantJson, Restaurant.class);
+    }
+
+    private static Branch createNode() {
+
+        Branch root = new Branch();
+        Branch branch1 = new Branch();
+        Branch branch2 = new Branch();
+
+
+        root.addNode(new Leaf("Novo texto"));
+        root.addNode(branch1);
+
+        branch1.addNode(new Leaf("de exemplo"));
+        branch1.addNode(new Leaf("para"));
+        branch1.addNode(branch2);
+
+        branch2.addNode(new Leaf("assemble"));
+        branch2.addNode(new Leaf("do documento. Horário: 12:18"));
+
+        return root;
+    }
+
+    public static void assemble(Node root) {
+
+        Assembler documentAssembler = new Assembler();
+
+        documentAssembler.assemble(root);
+        System.out.println(documentAssembler.getText());
+    }
+
+    private static Branch serializeTree(Node root) {
+
+        RuntimeTypeAdapterFactory<Node> typeAdapterFactory = RuntimeTypeAdapterFactory
+                .of(Node.class, "Node Type")
+                .registerSubtype(Branch.class, "Branch")
+                .registerSubtype(Leaf.class, "Leaf");
+
+        Gson modifiedGson = new GsonBuilder().registerTypeAdapterFactory(typeAdapterFactory).create();
+
+        String output = modifiedGson.toJson(root);
+        JsonObject test = (JsonObject) JsonParser.parseString(output);
+
+//        GsonBuilder gsonBuilder = new GsonBuilder();
+//        gsonBuilder.registerTypeAdapter(Node.class, new NodeDeserializer());
+//        Gson gson2 = gsonBuilder.create();
+
+        return modifiedGson.fromJson(test, Branch.class);
     }
 }
